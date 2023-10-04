c90e9f38e41194a09f23cc1c517e24e117a0e668
FagnerAmorim <fuinha.fagner2008@hotmail.com>
2023-10-04 19:25:50 -0300

    atualização das etapas

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
 var cardapio = {};
 
 var MEU_CARRINHO = [];
+var MEU_ENDERECO = null;
+
+var VALOR_CARRINHO = 0;
+var VALOR_ENTREGA = 5;
 
 cardapio.eventos = {
 
@@ -146,8 +150,8 @@ cardapio.metodos = {
         }
 
         else {
-            $(".botao-carrinho").addClassClass('hidden');
-            $(".container-total-carrinho").addClassClass('hidden');
+            $(".botao-carrinho").addClass('hidden');
+            $(".container-total-carrinho").addClass('hidden');
         }
 
         $(".badge-total-carrinho").html(total);
@@ -159,31 +163,13 @@ cardapio.metodos = {
 
         if (abrir) {
             $("#modalCarrinho").removeClass('hidden');
-            cardapio.metodos.carregarEtapa(1);
+            cardapio.metodos.carregarCarrinho();
         }
         else {
             $("#modalCarrinho").addClass('hidden');
         }
     },
-
-    // mensagens
-    mensagem: (texto, cor = 'red', tempo = 3500) => {
-
-        let id = Math.floor(Date.now() * Math.random()).toString();
-
-        let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
-
-        $("#container-mensagens").append(msg);
-
-        setTimeout(() => {
-            $("#msg-" + id).removeClass('fadeInDown');
-            $("#msg-" + id).addClass('fadeOutUp');
-            setTimeout(() => {
-                $("#msg-" + id).remove();
-            },800);
-        },tempo)
-    },
-
+   
     // altera os textos e exibe os botoes das etapas
     carregarEtapa: (etapa) => {
         if (etapa == 1) {
@@ -242,7 +228,242 @@ cardapio.metodos = {
         let etapa = $(".etapa.active").length;
         cardapio.metodos.carregarEtapa(etapa - 1);
 
-    }
+    },
+
+    // Carrega a lista de itens do carrinho
+    carregarCarrinho: () => {
+        cardapio.metodos.carregarEtapa(1);
+
+        if (MEU_CARRINHO.length > 0) {
+
+            $("#itensCarrinho").html('');
+
+            $.each(MEU_CARRINHO, (i, e) => {
+
+                let temp = cardapio.templates.itemCarrinho.replace(/\${img}/g, e.img)
+                .replace(/\${nome}/g, e.name)
+                .replace(/\${preco}/g, e.price.toFixed(2).replace('.',','))
+                .replace(/\${id}/g, e.id)
+                .replace(/\${qntd}/g, e.qntd)
+
+                $("#itensCarrinho").append(temp);
+
+                // ultimo item
+                if ((i + 1) == MEU_CARRINHO.length) {
+                    cardapio.metodos.carregarValores();
+                }
+
+            })
+
+        } else {
+            $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i> Seu carrinho está vazio.</p>');
+            cardapio.metodos.carregarValores();
+
+        }
+    },
+
+    // diminuir quantidade do item no carrinho
+    diminuirQuantidadeCarrinho: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+
+        if (qntdAtual > 1) {
+            $("#qntd-carrinho-" + id).text(qntdAtual - 1);
+            cardapio.metodos.atualizarCarrinho(id, qntdAtual - 1);
+
+        } else {
+            cardapio.metodos.removerItemCarrinho(id)
+        }
+
+    },
+
+    // aumentar quantidade do item no carrinho
+    aumentarQuantidadeCarrinho: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+        $("#qntd-carrinho-" + id).text(qntdAtual + 1);
+        cardapio.metodos.atualizarCarrinho(id, qntdAtual + 1);
+
+    },
+
+    // botão remover item do carrinho
+    removerItemCarrinho: (id) => {
+
+        MEU_CARRINHO = $.grep(MEU_CARRINHO, (e, i) => { return e.id != id});
+        cardapio.metodos.carregarCarrinho();
+
+        // atualiza o botão carrinho com a quantidade atualizada
+        cardapio.metodos.atualizarBadgeTotal();
+
+    },
+
+    // atualiza o carrinho com a quantidade atual
+    atualizarCarrinho: (id, qntd) => {
+
+        let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+        MEU_CARRINHO[objIndex].qntd = qntd;
+
+        // atualiza o botão carrinho com a quantidade atualizada
+        cardapio.metodos.atualizarBadgeTotal();
+        
+        // atualiza os valores (R$) totais do carrinho
+        cardapio.metodos.carregarValores();
+    },
+
+    // carrega os valores de subtotal, entrega e total
+    carregarValores: () => {
+
+        VALOR_CARRINHO = 0;
+
+        $("#lblSubTotal").text('R$ 0,00');
+        $("#lblValorEntrega").text('+ R$ 0,00');
+        $("#lblValorTotal").text('R$ 0,00');
+
+        $.each(MEU_CARRINHO, (i, e) => {
+
+            VALOR_CARRINHO += parseFloat(e.price * e.qntd);
+
+            if ((i + 1) == MEU_CARRINHO.length) {
+                $("#lblSubTotal").text(`R$ ${VALOR_CARRINHO.toFixed(2).replace('.', ',')}`);
+                $("#lblValorEntrega").text(`+ R$ ${VALOR_ENTREGA.toFixed(2).replace('.',',')}`);
+                $("#lblValorTotal").text(`R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.',',')}`);
+            }
+        })
+
+    },
+
+    // carregar a etapa endereços 
+    carregarEndereco: () => {
+
+        if (MEU_CARRINHO.length <= 0) {
+            cardapio.metodos.mensagem('Seu carrinho está vazio.')
+            return;
+        }
+
+        cardapio.metodos.carregarEtapa(2);
+    },
+
+    // API ViaCEP
+    buscarCep: () => {
+
+        // cria a variavel com o valor do cep
+        var cep = $("#txtCEP").val().trim().replace(/\D/g, '');
+
+        // verifica se o cep possui valor informado
+        if (cep != "") {
+
+            // Expressão regular para validar o cep utilizando o regex
+            var validarcep = /^[0-9]{8}$/;
+
+            if (validarcep.test(cep)) {
+
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+                if (!("erro" in dados)) {
+
+                    // Atualizar os campos com os valores retornados
+                    $("#txtEndereco").val(dados.logradouro);
+                    $("#txtBairro").val(dados.bairro);
+                    $("#txtCidade").val(dados.localidade);
+                    $("#ddlUf").val(dados.uf);
+                    $("#txtNumero").focus();
+
+                } else {
+                    cardapio.metodos.mensagem('CEP não encontrado. Preencha as informações manualmente.');
+                    $("#txtEndereco").focus();
+                }
+
+                })
+
+            } else {
+                cardapio.metodos.mensagem('Formato do CEP inválido.');
+                $("#txtCEP").focus();
+            }
+
+        } else {
+            cardapio.metodos.mensagem('Informe o CEP, por favor.');
+            $("#txtCEP").focus();
+        }
+    },
+
+    // Validação antes de prosseguir para a etapa 3
+    resumoPedido: () => {
+
+        let cep = $("#txtCEP").val().trim();
+        let endereco = $("#txtEndereco").val().trim();
+        let bairro = $("#txtBairro").val().trim();
+        let cidade = $("#txtCidade").val().trim();
+        let uf = $("#ddlUf").val().trim();
+        let numero = $("#txtNumero").val().trim();
+        let complemento = $("#txtComplemento").val().trim();
+
+        if (cep.length <= 0) {
+            cardapio.metodos.mensagem('Informe o CEP por favor.');
+            $("#txtEndereco").focus();
+            return;
+        } 
+
+        if (endereco.length <= 0) {
+            cardapio.metodos.mensagem('Informe o Endereço por favor.');
+            $("#txtCEP").focus();
+            return;
+        } 
+
+        if (bairro.length <= 0) {
+            cardapio.metodos.mensagem('Informe o Bairro por favor.');
+            $("#txtBairro").focus();
+            return;
+        } 
+
+        if (cidade.length <= 0) {
+            cardapio.metodos.mensagem('Informe a Cidade por favor.');
+            $("#txtCidade").focus();
+            return;
+        } 
+
+        if (uf.length == "-1") {
+            cardapio.metodos.mensagem('Informe a UF por favor.');
+            $("#ddlUF").focus();
+            return;
+        } 
+
+        if (numero.length <= 0) {
+            cardapio.metodos.mensagem('Informe o Número por favor.');
+            $("#txtNumero").focus();
+            return;
+        } 
+
+        MEU_ENDERECO = {
+            cep: cep,
+            endereco: endereco,
+            bairro: bairro,
+            cidade: cidade,
+            uf: uf,
+            numero: numero,
+            complemento: complemento
+        }
+
+        cardapio.metodos.carregarEtapa(3);
+        
+    },
+
+    // mensagens
+    mensagem: (texto, cor = 'red', tempo = 3500) => {
+
+        let id = Math.floor(Date.now() * Math.random()).toString();
+
+        let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+
+        $("#container-mensagens").append(msg);
+
+        setTimeout(() => {
+            $("#msg-" + id).removeClass('fadeInDown');
+            $("#msg-" + id).addClass('fadeOutUp');
+            setTimeout(() => {
+                $("#msg-" + id).remove();
+            },800);
+        },tempo)
+    },
  
 }
 
@@ -268,6 +489,24 @@ cardapio.templates = {
                 </div>
             </div>
         </div>
+    `,
+
+    itemCarrinho: `
+    <div class="col-12 item-carrinho">
+        <div class="img-produto">
+            <img src="\${img}">
+        </div>
+        <div class="dados-produto">
+            <p class="title-produto"><strong>\${nome}</strong></p>
+            <p class="price-produto"><strong>R$ \${preco}</strong></p>
+        </div>
+        <div class="add-carrinho">
+            <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fas fa-minus"></i></span>
+            <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
+            <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></span>
+            <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fa fa-times"></i></span>
+        </div>
+    </div>
     `
 
 }
