ef822a9e2c58f0b01b560767a6fde2b03a418e95
FagnerAmorim <fuinha.fagner2008@hotmail.com>
2023-10-09 18:26:26 -0300

    animação e responsividade

--- js/app.js
+++ js/app.js
@@ -583,7 +583,7 @@ cardapio.metodos = {
 cardapio.templates = {
 
     item: `
-        <div class="col-3 mb-5">
+        <div class="col-3 mb-5 animated fadeInUp">
             <div class="card card-item" id="\${id}">
                 <div class="img-produto">
                     <img src="\${img}" />
