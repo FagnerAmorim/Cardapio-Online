403ffdf5e7ffe942bff32b9ec5e843bdb9fe0510
FagnerAmorim <fuinha.fagner2008@hotmail.com>
2023-10-11 18:33:06 -0300

    finalizado responssividade da tela principal

--- js/app.js
+++ js/app.js
@@ -583,7 +583,7 @@ cardapio.metodos = {
 cardapio.templates = {
 
     item: `
-        <div class="col-3 mb-5 animated fadeInUp">
+        <div class="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 wow fadeInUp">
             <div class="card card-item" id="\${id}">
                 <div class="img-produto">
                     <img src="\${img}" />
